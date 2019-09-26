573fd0f52b955f8167bce0a5a3b1107a1ed87c36
Anthony <anthonyu234@gmail.com>
2019-09-26 06:35:39 +0100

    create tour model

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ mongoose
   })
   .then(() => console.log('Db connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
